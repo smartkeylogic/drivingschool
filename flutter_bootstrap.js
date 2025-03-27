eeb437fd714cb9fa5740f7cbcb4f7094428bfc90
smartkeylogic <dreams.dmroads@gmail.com>
2025-03-27 11:52:31 +1100

    New version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3239957470"
+    serviceWorkerVersion: "3331211384"
   }
 });
